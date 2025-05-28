ba255b6ddf51d797d0288ef248a14e017cf226fd
Alebrije <alephtav93@gmail.com>
2025-05-28 16:54:02 -0500

    fix: simplify vite config and downgrade vite version

---
+++ alebrije-website/alebrije-frontend/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'esnext',
+    sourcemap: true,
+    minify: false,
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    }
+  },
+  resolve: {
+    alias: {
+      process: "process/browser",
+      stream: "stream-browserify",
+      zlib: "browserify-zlib",
+      util: 'util'
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  }
+}) 
