4aa45e2f9671572107f23733c8c42bb1aa40ab74
Alebrije <alephtav93@gmail.com>
2025-05-28 16:26:41 -0500

    fix: add vite config for Solana dependencies

---
+++ alebrije-website/alebrije-frontend/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '@solana/wallet-adapter-wallets',
+        '@solana/wallet-adapter-react',
+        '@solana/wallet-adapter-react-ui',
+        '@solana/wallet-adapter-base',
+        '@solana/web3.js'
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      process: "process/browser",
+      stream: "stream-browserify",
+      zlib: "browserify-zlib",
+      util: 'util'
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  }
+}) 
