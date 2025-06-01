a92c0d24f125570a8c449019fc7c1c0a5df78d97
Alebrije <alephtav93@gmail.com>
2025-06-01 12:10:02 -0500

    Fix: X icon size to match other social media buttons for perfect symmetry

--- alebrije-website/alebrije-frontend/src/components/icons/XIcon.tsx
+++ alebrije-website/alebrije-frontend/src/components/icons/XIcon.tsx
@@ -1,11 +1,12 @@
 // XIcon.tsx
-const XIcon = ({ size = 24 }: { size?: number }) => (
+const XIcon = () => (
     <svg
-        height={size}
-        width={size}
+        height="1em"
+        width="1em"
         viewBox="0 0 24 24"
         fill="currentColor"
         xmlns="http://www.w3.org/2000/svg"
+        style={{ fontSize: 'inherit' }}
     >
         <title>X (formerly Twitter)</title>
         <path d="M17.53 2H21L14.3 10.1L22.32 22H15.45L10.53 15.3L4.94 22H1L8.1 13.4L0.36 2H7.45L11.84 8.1L17.53 2ZM16.32 20H18.17L6.09 4H4.1L16.32 20Z" />
